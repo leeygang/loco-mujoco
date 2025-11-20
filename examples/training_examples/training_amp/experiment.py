15d34aed2eac30319258c9ba1be49e4a4150e51d
leeygang <--local>
2025-11-19 21:43:18 -0800

    update config to fix crash

--- examples/training_examples/training_amp/experiment.py
+++ examples/training_examples/training_amp/experiment.py
@@ -5,8 +5,10 @@ import jax.numpy as jnp
 import wandb
 from dataclasses import fields
 from loco_mujoco import TaskFactory
-from loco_mujoco.algorithms import PPOJax
+from loco_mujoco.algorithms import AMPJax
 from loco_mujoco.utils.metrics import QuantityContainer
+from loco_mujoco.utils import MetricsHandler
+from loco_mujoco.trajectory import Trajectory
 
 import hydra
 from hydra.core.hydra_config import HydraConfig
@@ -42,11 +44,29 @@ def experiment(config: DictConfig):
         # create env
         env = factory.make(**config.experiment.env_params, **config.experiment.task_factory.params)
 
+        # create expert dataset
+        expert_dataset_path = os.path.join(result_dir, "expert_dataset.npz")
+        if not os.path.exists(expert_dataset_path):
+            # if it doesn't exist, create it from the trajectory
+            expert_dataset = env.create_dataset()
+            new_traj = Trajectory(info=env.th.traj.info, data=env.th.traj.data,
+                                  obs_container=env.obs_container, transitions=expert_dataset)
+            new_traj.save(expert_dataset_path)
+        else:
+            # if it exists, load it
+            new_traj = Trajectory.load(expert_dataset_path)
+            env.load_trajectory(new_traj)
+            expert_dataset = env.create_dataset()
+
         # get initial agent configuration
-        agent_conf = PPOJax.init_agent_conf(env, config)
+        agent_conf = AMPJax.init_agent_conf(env, config)
+        agent_conf = agent_conf.add_expert_dataset(expert_dataset)
+
+        # setup metric handler (optional)
+        mh = MetricsHandler(config, env) if config.experiment.validation.active else None
 
         # build training function
-        train_fn = PPOJax.build_train_fn(env, agent_conf)
+        train_fn = AMPJax.build_train_fn(env, agent_conf, mh=mh)
 
         # jit and vmap training function
         train_fn = jax.jit(jax.vmap(train_fn)) if config.experiment.n_seeds > 1 else jax.jit(train_fn)
@@ -58,29 +78,34 @@ def experiment(config: DictConfig):
 
         # save agent state
         agent_state = out["agent_state"]
-        save_path = PPOJax.save_agent(result_dir, agent_conf, agent_state)
+        save_path = AMPJax.save_agent(result_dir, agent_conf, agent_state)
         run.config.update({"agent_save_path": save_path})
 
         import time
         t_start = time.time()
-        # Skip historical metric backfill if live streaming is active to avoid non-monotonic step warnings
+        # get the metrics and log them
         if not config.experiment.debug and not config.experiment.live_wandb:
             training_metrics = out["training_metrics"]
             validation_metrics = out["validation_metrics"]
 
+            # calculate mean across seeds
             training_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), training_metrics)
             validation_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), validation_metrics)
 
             for i in range(len(training_metrics.mean_episode_return)):
                 step_i = int(training_metrics.max_timestep[i])
                 run.log({"Mean Episode Return": training_metrics.mean_episode_return[i],
-                         "Mean Episode Length": training_metrics.mean_episode_length[i]}, step=step_i)
+                         "Mean Episode Length": training_metrics.mean_episode_length[i],
+                         "Discriminator/Output Policy": training_metrics.discriminator_output_policy[i],
+                         "Discriminator/Output Expert": training_metrics.discriminator_output_expert[i]},
+                        step=step_i)
 
                 if (i + 1) % config.experiment.validation_interval == 0 and config.experiment.validation.active:
                     run.log({"Validation Info/Mean Episode Return": validation_metrics.mean_episode_return[i],
                              "Validation Info/Mean Episode Length": validation_metrics.mean_episode_length[i]},
                             step=step_i)
 
+                    # log all measures
                     metrics_to_log = {}
                     for field in fields(validation_metrics):
                         attr = getattr(validation_metrics, field.name)
@@ -91,8 +116,10 @@ def experiment(config: DictConfig):
                                 attr_value = getattr(attr, attr_name)
                                 if attr_value.size > 0:
                                     metrics_to_log[f"Validation Measures/{measure_name}/{attr_name}"] = attr_value[i]
+
                     run.log(metrics_to_log, step=step_i)
 
+                    # metric for used for wandb sweep (optional)
                     site_rpos = validation_metrics.euclidean_distance.site_rpos[i]
                     site_rrotvec = validation_metrics.euclidean_distance.site_rpos[i]
                     site_rvel = validation_metrics.euclidean_distance.site_rpos[i]
@@ -102,7 +129,7 @@ def experiment(config: DictConfig):
 
         # run the environment with the trained agent to record video
         try:
-            PPOJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=200, n_envs=20, record=True,
+            AMPJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=200, n_envs=20, record=True,
                                train_state_seed=0)
             video_file = env.video_file_path
             run.log({"Agent Video": wandb.Video(video_file)})
